6af55c6435a13203789076d3d484b91a5239eb45
Pablo Horno <pablojhorno@gmail.com>
2021-03-30 02:53:03 +0200

    init commit

---
+++ src/test/java/com/example/javarestservice/JavaRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
